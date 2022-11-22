1b3611852eb399893be1793ae109f62bc6eb9380
Yura Mishin <robothot@yandex.ru>
2022-11-22 04:55:00 +0300

    fix: внесены правки в работу слайдера

--- src/js/app.js
+++ src/js/app.js
@@ -13,3 +13,58 @@ import './modules/accordeon.js';
 import './modules/closelist.js';
 import './modules/form.js';
 import './modules/upbtn.js';
+
+// if (document.querySelector('.control__save')) {
+//   const savebtn = document.querySelector('.control__save');
+//   console.log(savebtn)
+//   savebtn.classList.add('kdd')
+
+//   savebtn.addEventListener('click', () => {
+//     savebtn.classList.toggle('--active');
+//   });
+// }
+
+if (document.querySelector('.swiper-slide')) {
+  const slides = document.querySelectorAll('.swiper__slide');
+  const pervBox = document.querySelector('.card-content__perview');
+  const nextBtnPr = document.querySelector('.slider-button-next');
+  const prevBtnPr = document.querySelector('.slider-button-prev');
+  const nextBtn = document.querySelector('.swiper-button-next');
+  const prevBtn = document.querySelector('.swiper-button-prev');
+
+  const showSlide = (slide) => {
+    pervBox.children[1].setAttribute('src', slide.target.getAttribute('src'));
+  };
+
+  const slideActive = () => {
+    for (let i = 0; i < slides.length; i++) {
+      if (slides[i].offsetParent.classList.contains('swiper-slide-active')) {
+        pervBox.children[1].setAttribute('src', slides[i].children[0].getAttribute('src'));
+      }
+    }
+  };
+
+  const increaseZoom = (img) => {
+    img.target.style.objectFit = 'none';
+    img.target.style.objectPosition = `-${img.layerX}px -${img.layerY}px`;
+  };
+
+  for (let i = 0; slides.length > i; i++) {
+    slides[i].addEventListener('mouseover', showSlide);
+  }
+
+  pervBox.addEventListener('mousemove', increaseZoom);
+  pervBox.addEventListener('mouseout', () => {
+    pervBox.children[1].removeAttribute('style');
+  });
+
+  prevBtnPr.addEventListener('click', () => {
+    prevBtn.click();
+    slideActive();
+  });
+
+  nextBtnPr.addEventListener('click', () => {
+    nextBtn.click();
+    slideActive();
+  });
+}
--- src/js/modules/swiper.js
+++ src/js/modules/swiper.js
@@ -3,59 +3,33 @@
 import Swiper, { Autoplay, Navigation, Pagination, Scrollbar, EffectCoverflow } from 'swiper';
 Swiper.use([Autoplay, Navigation, Pagination, Scrollbar, EffectCoverflow]);
 
-const slider = new Swiper('.slider__inner', {
+const switcher = new Swiper('.card-content__swiper', {
   loop: true,
-  centeredSlides: true,
-  spaceBetween: 5,
-  effect: 'coverflow',
-  coverflowEffect: {
-    rotate: 0,
-    depth: 300,
-    slideShadows : false,
-  },
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+
   breakpoints: {
     320: {
       slidesPerView: 1,
+      spaceBetween: 0,
+      direction: 'horizontal',
     },
-    576: {
+    700: {
+      centeredSlides: true,
       slidesPerView: 1.5,
+      spaceBetween: 10,
+      direction: 'horizontal',
     },
     1024: {
-      slidesPerView: 1,
+      slidesPerView: 5,
+      spaceBetween: 20,
+      direction: 'vertical',
     },
   }
 });
 
-const switcher = new Swiper('.switch__inner', {
-  direction: 'vertical',
-  loop: true,
-  slidesPerView: 5.5,
-  spaceBetween: 115,
-  navigation: {
-    nextEl: '.switch__next',
-    prevEl: '.switch__prev',
-  },
-});
-
-const switchSlides = document.querySelectorAll('.switch__slide');
-
-for (let item of switchSlides) {
-  item.addEventListener('click', () => {
-    for (let i of switchSlides) {
-      i.classList.remove('switch__slide--active');
-    }
-    item.classList.add('switch__slide--active');
-    for (let i = 0; i < slider.slides.length; i++) {
-      if (item.closest('.swiper-slide').getAttribute('slide-id') === slider.slides[i].attributes[1].value) {
-        slider.slideToLoop(slider.slides[i].dataset.swiperSlideIndex, 800);
-      }
-    }
-  });
-}
 
 const swiper = new Swiper('.similar__slider', {
   direction: 'horizontal',
